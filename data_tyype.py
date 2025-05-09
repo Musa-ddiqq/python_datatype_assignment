4bd162a89346afb96ac1aa3a81c45662de8cfa95
Musaddiq Qureshi <musaddiqqureshii@gmail.com>
2025-05-09 23:32:17 +0500

    Add files via upload

---
+++ data_tyype.py
@@ -0,0 +1,23 @@
+# Integer variables 
+age = 21                  # person's age in years
+score = 83                # test score out of 100
+print("Age:", age)
+print("Score:", score)
+
+# String variables 
+name = "musa"             #  a person's name
+message = "Learning Python is fun!"  #   motivational message
+print("Name:", name)
+print("Message:", message)
+
+# Float variables 
+height = 1.75             #  height in meters
+temperature = 36.7        #  body temperature in Celsius
+print("Height:", height, "meters")
+print("Temperature:", temperature, "°C")
+
+# Boolean variables
+is_student = True         # Indicates if person is a student
+has_graduated = False     # graduation status
+print("Is student?", is_student)
+print("Has graduated?", has_graduated)
